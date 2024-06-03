9bda88aa7d2abfc4ae06cffbea17163bad6ceb8b
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-06-04 03:56:12 +0900

    2-7 完了

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// 2-6
+
 const name = "hello";
 
 let nameChange = "hello";
@@ -35,6 +37,27 @@ let nameObj3: NAME3 = { first: "Yamada", last: null };
 
 const func1 = (x: number, y: number): number => {
   return x + y;
+};
+
+// 2-7
+
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy",
 };
 
 function App() {
