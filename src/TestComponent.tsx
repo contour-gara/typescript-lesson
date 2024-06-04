945cde1292886929daf850f5b14dd7bfe1787c62
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-06-05 01:40:49 +0900

    2-17 完了

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 // 2-16
+// 2-17
 
 interface Props {
   text: string;
 }
 
-export const TestComponent: React.FC<Props> = (props) => {
+interface UserData {
+  id: number;
+  name: string;
+}
+
+const TestComponent: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(null);
+  const [user, setUser] = useState<UserData>({ id: 1, name: "dummy" });
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1>{count}</h1>
+      <h1>{user.name}</h1>
     </div>
   );
 };
